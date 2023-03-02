4b37db1d63865a02aacb9dd60920558042c3446a
shahid <shahdbest@gmail.com>
2023-03-02 15:05:12 +0530

    question

---
+++ src/main/java/com/stackroute/strings/Anagram.java
@@ -0,0 +1,8 @@
+package com.stackroute.strings;
+
+public class Anagram {
+    //write logic to check given two phrases are anagrams or not and return result
+    public String checkAnagrams(String phraseOne, String phraseTwo) {
+        return null;
+    }
+}
